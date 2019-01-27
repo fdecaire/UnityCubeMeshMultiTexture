05cf2cd6b1f89ca0570281aea4809a5a81fd6c92
Frank DeCaire <frank@decaire.net>
2019-01-27 18:21:26 +0000

    Refactored to push vertices, triangles and uvs into method

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -9,19 +9,15 @@ public class Main : MonoBehaviour
     void Start()
     {
         var gameObjects = new List<GameObject>();
-        var meshList = new List<Mesh>();
 
-        //TODO: create array of meshes
         //TODO: break vertices, triangles, uvs into each mesh
-        //TODO: render at the end
 
-        // mesh filter
-        meshList.Add(AddMeshFilter(gameObjects, "ceil014"));
-        meshList.Add(AddMeshFilter(gameObjects, "door13_0"));
+
 
 
         float size = 1f;
-        Vector3[] vertices = {
+        Vector3[] vertices =
+        {
             // left
             new Vector3(0, 0, size),
             new Vector3(0, size, size),
@@ -59,62 +55,135 @@ public class Main : MonoBehaviour
             new Vector3(size, 0, 0),
         };
 
-        int[] triangles = {
-            0,1,2, // left
-            2,3,0,
-            4,5,6, // top
-            6,7,4,
-            8,9,10, // right
-            10,11,8,
-            12,13,14, // bottom
-            14,15,12,
-            16,17,18, // back
-            18,19,16,
-            20,21,22, // front
-            22,23,20
+        Vector3[] vertices0 =
+        {
+            // left
+            new Vector3(0, 0, size),
+            new Vector3(0, size, size),
+            new Vector3(0, size, 0),
+            new Vector3(0, 0, 0),
+        };
+
+        Vector3[] vertices1 =
+        {
+            // top
+            new Vector3(0, size, 0),
+            new Vector3(0, size, size),
+            new Vector3(size, size, size),
+            new Vector3(size, size, 0),
+        };
+
+        Vector3[] vertices2 =
+        {
+            // right
+            new Vector3(size, 0, 0),
+            new Vector3(size, size, 0),
+            new Vector3(size, size, size),
+            new Vector3(size, 0, size),
+        };
+
+        Vector3[] vertices3 =
+        {
+            // bottom
+            new Vector3(0, 0, size),
+            new Vector3(0, 0, 0),
+            new Vector3(size, 0, 0),
+            new Vector3(size, 0, size),
+        };
+
+        Vector3[] vertices4 =
+        {
+            // back
+            new Vector3(size, 0, size),
+            new Vector3(size, size, size),
+            new Vector3(0, size, size),
+            new Vector3(0, 0, size),
+        };
+
+        Vector3[] vertices5 =
+        {
+            // front
+            new Vector3(0, 0, 0),
+            new Vector3(0, size, 0),
+            new Vector3(size, size, 0),
+            new Vector3(size, 0, 0),
+        };
+
+        int[] triangles =
+        {
+            0, 1, 2, // left
+            2, 3, 0,
+            4, 5, 6, // top
+            6, 7, 4,
+            8, 9, 10, // right
+            10, 11, 8,
+            12, 13, 14, // bottom
+            14, 15, 12,
+            16, 17, 18, // back
+            18, 19, 16,
+            20, 21, 22, // front
+            22, 23, 20
+        };
+
+        int[] triangles0 =
+        {
+            0, 1, 2, // left
+            2, 3, 0,
+        };
+
+        Vector2[] uvs =
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
+        };
+
+        // these are all the same for now
+        Vector2[] uvs0 =
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(1, 1),
+            new Vector2(0, 1),
         };
 
 
-        Vector2[] uvs = {
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
 
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(1, 1),
-            new Vector2(0, 1),
-        };
-
-        meshList[0].Clear();
-        meshList[0].vertices = vertices;
-        meshList[0].triangles = triangles;
-        meshList[0].uv = uvs;
-        meshList[0].RecalculateNormals();
+        AddMeshFilter(gameObjects, "ceil014", vertices0, triangles0, uvs0);
+        AddMeshFilter(gameObjects, "door13_0", vertices1, triangles0, uvs0);
+        AddMeshFilter(gameObjects, "ceil014", vertices2, triangles0, uvs0);
+        AddMeshFilter(gameObjects, "ceil014", vertices3, triangles0, uvs0);
+        AddMeshFilter(gameObjects, "ceil014", vertices4, triangles0, uvs0);
+        AddMeshFilter(gameObjects, "ceil014", vertices5, triangles0, uvs0);
     }
 
-    private Mesh AddMeshFilter(List<GameObject> o, string textureName)
+    private void AddMeshFilter(List<GameObject> o, string textureName, Vector3[] vertices, int[] triangles, Vector2[] uvs)
     {
         o.Add(new GameObject());
         Instantiate(o[o.Count-1]);
@@ -140,6 +209,11 @@ public class Main : MonoBehaviour
 
         meshRenderer.material = material;
 
-        return mesh;
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
     }
 }
